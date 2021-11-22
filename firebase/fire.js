53fa6bdca5c5c82995de7b021902a9bd32810baf
M-A-N-O-J-R <manojravichandran12@gmail.com>
2021-11-22 23:17:16 +0530

    fire.js updated

---
+++ firebase/fire.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA5JabX3yv5cRMqEwF6Ebq-VBfDLxuN5lk",
+    authDomain: "gossip-5f3ba.firebaseapp.com",
+    databaseURL :'https://gossip-5f3ba.firebaseio.com',
+    projectId: "gossip-5f3ba",
+    storageBucket: "gossip-5f3ba.appspot.com",
+    messagingSenderId: "219284175146",
+    appId: "1:219284175146:web:6b73f55a8dc68a36de0653",
+    measurementId: "G-3XMB7CL2W0"
+  };
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
