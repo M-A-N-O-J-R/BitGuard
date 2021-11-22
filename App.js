b51d9f6dc835694564d4ec55eed513ea2316c985
M-A-N-O-J-R <manojravichandran12@gmail.com>
2021-11-22 22:32:44 +0530

    second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Gossip App</Text>
+      <Text>Gossip App main branch</Text>
       <StatusBar style="auto" />
     </View>
   );
