8aed7d76bf046fb996bdbb245c56870038d305d5
M-A-N-O-J-R <manojravichandran12@gmail.com>
2021-11-23 17:05:58 +0530

    google working

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -3,6 +3,11 @@ import React,{useState} from 'react';
 import { StyleSheet, Text, View ,TextInput,TouchableOpacity,Button,KeyboardAvoidingView} from 'react-native';
 import firebase from '../firebase/fire';
 
+import * as Google from 'expo-google-app-auth';
+import { AntDesign } from '@expo/vector-icons';
+
+
+
 
 
 export default function SignupScreen({navigation}) {
@@ -10,6 +15,59 @@ export default function SignupScreen({navigation}) {
   const [email,setEmail]=useState("");
   const [password,setPassword]=useState("");
   const [Error,setError]=useState('');
+  const [googleSubmitting,setGoogleSubmitting]=useState(false);
+
+
+  const handleGoogleSignin=()=>{
+    const config= {
+      iosClientId:`219284175146-6f1tq9uk0a6r76tjte6e6df59sqnj0dn.apps.googleusercontent.com`,
+      androidClientId:`219284175146-bvngane5ssmujvteiosg672mpdof6r5a.apps.googleusercontent.com`,
+      scopes: ['profile', 'email']
+    }
+    Google.logInAsync(config).then((result)=>
+    {
+      const {type,user} = result;
+      if(type == 'success')
+      {
+        const {email,name,photoUrl}=user;
+        console.log('google signin success');
+        setTimeout(()=> navigation.navigate('Home',{email,name,photoUrl}),1000);
+      }
+      else
+      {
+        console.log('google signin failed');
+      }
+    }).catch((err)=>
+    {
+      console.error(err.message);
+    })
+  }
+
+
+
+
+  const signInWithGoogle =  async() =>{
+    try {
+      const result = await Google.logInAsync({
+        behavior:'web',
+        androidClientId: '219284175146-g4ltjbim9spqhiiul2j7r7onb66b3rnh.apps.googleusercontent.com',
+        iosClientId: '219284175146-mer84s58cr8gl9t2potaglt7tehs68ul.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+      });
+  
+      if (result.type === 'success') {
+        this.onSignIn(result);
+        return result.accessToken;
+        
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  }
+
+
 
   const signUp = async()=>{
     
@@ -38,6 +96,11 @@ export default function SignupScreen({navigation}) {
         </TouchableOpacity>
         <TouchableOpacity onPress={()=>{signUp()}} style={styles.button2}>
            <Text style={styles.btnText2} >register</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity onPress={()=>handleGoogleSignin()}>
+            <AntDesign name="google" size={24} color="black" />    
         </TouchableOpacity>
       </View>
     </View>
