a2885b9a2a8774f1ee5a38d0dc766841d8edf98d
aravintharaj-s <aravioct@gmail.com>
2022-01-27 19:38:49 +0530

    added note and dropdown

--- screens/AddItem.js
+++ screens/AddItem.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   Alert,
   ImageBackground,
+  Picker
 } from 'react-native';
 import firebase from '../firebase/fire'
 const ref=firebase.firestore().collection('records');
@@ -21,34 +22,52 @@ export default function AddItem ({navigation}){
  const [title, onChangeTitle] = React.useState('');
  const [username, onChangeUsername] = React.useState('');
  const [pass, onChangePass] = React.useState('');
+ const [note, onChangeNote] = React.useState('');
 
- let addItem = (title,username,pass) => {
+ let addItem = (title,username,pass,note) => {
   ref.add({
     name: title,
     id: username,
-    password: pass
+    password: pass,
+    Note:note
   });
   
 };
 
 const  handleSubmit = () => {
-    addItem(title,username,pass);
+    addItem(title,username,pass,note);
     Alert.alert('Item saved successfully','Do you wish to add more items',[
       {text:'Yes',onPress:() => {}},
       {text:'No',onPress:() => {navigation.navigate('Home');}}
     ]);
     
   };
+  const [selectedValue, setSelectedValue] = useState("java");
     return (
       <ImageBackground source={addbg} style={styles.bgimg}>
       <View style={styles.main}>
         <Text style={styles.title}>Drop your passwords here!</Text>
+        <Text>What type of item is this? </Text>
+        <View style={{backgroundColor:"gainsboro",width:170,borderRadius:10,padding:10}}>
+        <Picker
+        selectedValue={selectedValue}
+        style={{ height: 25, width: 170, }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Login" value="Login" />
+        <Picker.Item label="Card" value="Card" />
+        <Picker.Item label="Identity" value="Identity" />
+        <Picker.Item label="Secure Note" value="Secure Note" />
+      </Picker>
+      </View>
         <Text>Website/App : </Text>
         <TextInput placeholder="www.gmail.com" style={styles.itemInput} onChangeText={text => onChangeTitle(text)} />
         <Text>Username : </Text>
         <TextInput placeholder="xyz" style={styles.itemInput} onChangeText={text => onChangeUsername(text)} />
         <Text>Password : </Text>
         <TextInput placeholder="***************"  value={pass} secureTextEntry style={styles.itemInput} onChangeText={text => onChangePass(text)} />
+        <Text>Note  :</Text>
+        <TextInput placeholder="(Optional)"style={[styles.itemInput,{height:250}]} onChangeText={text => onChangeNote(text)} />
         <TouchableHighlight
           style={styles.button}
           underlayColor="white"
@@ -111,5 +130,11 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent:'center',
     alignItems:'center'
+  },
+  pick:{
+     height: 50,
+      width: 150,
+       margin:10,
+       color:'black'
   }
 });
